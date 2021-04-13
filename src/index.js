45931221258c24875da6bfd37017de339cda6982
Godreigneth Levi <godreignethlevidibia@yahoo.com>
2021-04-13 21:45:34 +0200

    Cleaned up the React app

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+// eslint-disable-next-line
+import $ from 'jquery';
+// eslint-disable-next-line
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+// eslint-disable-next-line
+import CloseNavBarFix from "./CloseNavBarFix";
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
